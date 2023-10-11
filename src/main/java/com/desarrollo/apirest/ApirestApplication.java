bc6fe0cc8a55a66ebb68ec31fa094bb5c87f90f8
Tmonti11 <tommontigel@gmail.com>
2023-10-11 19:06:07 -0300

    parcial

--- src/main/java/com/desarrollo/apirest/ApirestApplication.java
+++ src/main/java/com/desarrollo/apirest/ApirestApplication.java
@@ -5,17 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApirestApplication {
-<<<<<<< HEAD
 
-=======
->>>>>>> ffb96d0 (parcial)
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApirestApplication.class, args);
 	}
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> ffb96d0 (parcial)
+
+
 }
